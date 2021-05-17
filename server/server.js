6f5a9ae0b61486bd75243e6add10a35d981eb882
Kenny Tran <kenny.tran138@gmail.com>
2021-05-17 18:21:13 -0500

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const app = express();
+
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../dist")));
+
+module.exports = app;
