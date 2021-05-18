0d797b0c2599d28287bd4261984965dba477e19c
Kenny Tran <kenny.tran138@gmail.com>
2021-05-18 09:45:24 -0500

    Implemented React Router for switching between homepage and history

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import * as React from "react";
 import EmoApp from "./components/EmoApp";
 import History from "./components/History";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function App() {
   return (
-    <>
-      <div>React is rendering!</div>
-      {/* <EmoApp /> */}
-      <History />
-    </>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/history">History</Link>
+          </li>
+        </ul>
+      </div>
+
+      <hr />
+
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <EmoApp />
+          </Route>
+          <Route path="/history">
+            <History />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
